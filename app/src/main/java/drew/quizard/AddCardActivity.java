d97023025582dc4a7ffd0d16f0b3ac1b8d933da1
dronk6 <6drewr@gmail.com>
2020-03-04 20:46:26 -0500

    Lab 2

---
+++ app/src/main/java/drew/quizard/AddCardActivity.java
@@ -0,0 +1,33 @@
+package drew.quizard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+        findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                data.putExtra("Question", ((EditText) findViewById(R.id.edit_question)).getText().toString()); // puts one string into the Intent, with the key as 'question'
+                data.putExtra("Answer", ((EditText) findViewById(R.id.edit_answer)).getText().toString()); // puts another string into the Intent, with the key as 'answer'
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
+    }
+}
--- app/src/main/java/drew/quizard/MainActivity.java
+++ app/src/main/java/drew/quizard/MainActivity.java
@@ -2,8 +2,10 @@ package drew.quizard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_one_question).setVisibility(View.INVISIBLE);
             }
         });
-}
+        findViewById(R.id.flashcard_one_answer_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_one_answer_one).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_one_question).setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 1);
+
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && data != null && resultCode == -1) {
+            String question = data.getExtras().getString("Question");
+            String answer = data.getExtras().getString("Answer");
+
+            ((TextView) findViewById(R.id.flashcard_one_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_one_answer_one)).setText(answer);
+
+        }
+    }
 }
