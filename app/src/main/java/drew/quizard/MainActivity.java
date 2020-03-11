979a002ef6d30c7613a4b581cfb2cc13581aff60
dronk6 <6drewr@gmail.com>
2020-03-11 19:13:58 -0400

    Lab 3

--- app/src/main/java/drew/quizard/MainActivity.java
+++ app/src/main/java/drew/quizard/MainActivity.java
@@ -7,45 +7,79 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.flashcard_one_question).setOnClickListener(new View.OnClickListener() {
+
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0){
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_one_answer_one).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_one_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
         });
-        findViewById(R.id.flashcard_one_answer_one).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_one_answer_one).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_one_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
         findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivityForResult(intent, 1);
+                MainActivity.this.startActivityForResult(intent, 100);
 
+            }
+        });
+        findViewById(R.id.next_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
             }
         });
 
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && data != null && resultCode == -1) {
+        if (requestCode == 100 && data != null && resultCode == -1) {
             String question = data.getExtras().getString("Question");
             String answer = data.getExtras().getString("Answer");
 
-            ((TextView) findViewById(R.id.flashcard_one_question)).setText(question);
-            ((TextView) findViewById(R.id.flashcard_one_answer_one)).setText(answer);
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
 
+            flashcardDatabase.insertCard(new Flashcard(question,answer));
+            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
+//testing github changes
